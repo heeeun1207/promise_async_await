4a4be5f95c649bc5d05882d7970704ec906fe0f8
jungheeeun <jungheeeun272@gmail.com>
2023-04-29 13:42:10 +0900

    .catch  .finally

--- promise.js
+++ promise.js
@@ -3,11 +3,18 @@ const promise = new Promise((resolve,reject)=>{
 	//doing some heavy work ( => networt, read file )
 console.log('doing something...')
 setTimeout(() => {
-	//resolve('heeun');
-	reject(new Error('no network'));
+	resolve('heeun');
+	//reject(new Error('no network'));
 	},2000);
 });
 //2.Consumers
-promise.then((value)=>{
+promise
+.then(value => { 
 	console.log(value);
+})
+.catch(error => {
+	console.log(error);
+})
+.finally(()=> { 
+	console.log('finally');
 });
