9629854cb77e8cca955d8d7d969232605b88d817
jungheeeun <jungheeeun272@gmail.com>
2023-04-29 13:31:13 +0900

    1. Promise 2.Consumers , Error object

--- promise.js
+++ promise.js
@@ -1,2 +1,13 @@
-//1 Producer 
-const promise = new Promise()
+//callback : result / reject 
+const promise = new Promise((resolve,reject)=>{
+	//doing some heavy work ( => networt, read file )
+console.log('doing something...')
+setTimeout(() => {
+	//resolve('heeun');
+	reject(new Error('no network'));
+	},2000);
+});
+//2.Consumers
+promise.then((value)=>{
+	console.log(value);
+});
